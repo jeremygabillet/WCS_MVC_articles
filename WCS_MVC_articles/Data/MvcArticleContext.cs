c5a8e71a48ea845df85d3915148b1522cd4892a8
jeremygabillet <jeremy.gabillet@gmail.com>
2021-11-01 23:07:39 +0100

    Finishing the MVC quest

---
+++ WCS_MVC_articles/Data/MvcArticleContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WCS_MVC_articles.Models;
+
+namespace WCS_MVC_articles.Data
+{
+    public class MvcArticleContext : DbContext
+    {
+        public MvcArticleContext(DbContextOptions<MvcArticleContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Article { get; set; }
+    }
+}
